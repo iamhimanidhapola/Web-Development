8413a930488231117d0ab3d282b5fcc9904982f6
iamhimanidhapola <himanidhapolacoding@gmail.com>
2025-02-05 12:52:30 +0530

    DOM

---
+++ DOM/domfun.js
@@ -0,0 +1,5 @@
+let element=document.createElement("div");
+element.setAttribute("class","created");
+document.querySelector(".container").append(element);
+document.querySelector(".box").innerText="Believe in yourself";
+document.querySelector(".container").insertAdjacentHTML("beforeend", "<b>hi i am HD</b>");
---
+++ DOM/script2.js
@@ -0,0 +1,13 @@
+obj={
+    1:Red,
+    2:pink ,
+    3:blue ,
+    4:hotpink,
+    5:green 
+}
+ 
+document.body.childNodes[1].firstElementChild.style.backgroundColor=obj['Math.floor(Math.random()*5)+1'];
+document.body.childNodes[1].secondElementChild.style.backgroundColor=obj['Math.floor(Math.random()*5)+1'];
+document.body.childNodes[1].firstElementChild.style.backgroundColor=obj['Math.floor(Math.random()*5)+1'];
+document.body.childNodes[1].firstElementChild.style.backgroundColor=obj['Math.floor(Math.random()*5)+1'];
+document.body.childNodes[1].firstElementChild.style.backgroundColor=obj['Math.floor(Math.random()*5)+1'];
